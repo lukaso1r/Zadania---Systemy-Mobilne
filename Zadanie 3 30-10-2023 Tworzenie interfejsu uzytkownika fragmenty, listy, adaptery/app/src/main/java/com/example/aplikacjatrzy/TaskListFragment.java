91ff7c97b4f41f4facac9aab35dea9a8ab25ae84
Łukasz Kaliszewicz <lukasz.kaliszewicz01@gmail.com>
2023-10-30 22:07:48 +0100

    21

--- Zadanie 3 30-10-2023 Tworzenie interfejsu uzytkownika fragmenty, listy, adaptery/app/src/main/java/com/example/aplikacjatrzy/TaskListFragment.java
+++ Zadanie 3 30-10-2023 Tworzenie interfejsu uzytkownika fragmenty, listy, adaptery/app/src/main/java/com/example/aplikacjatrzy/TaskListFragment.java
@@ -1,9 +1,26 @@
 package com.example.aplikacjatrzy;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class TaskListFragment extends Fragment {
 
+    RecyclerView recyclerView;
 
-
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_task_list, container, false);
+        recyclerView = view.findViewById(R.id.task_recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        return view;
+    }
 }
