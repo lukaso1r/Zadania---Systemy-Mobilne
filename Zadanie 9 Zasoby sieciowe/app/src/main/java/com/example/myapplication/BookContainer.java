4557189881055d5b0af0f83eff09412d5a45f829
Łukasz Kaliszewicz <lukasz.kaliszewicz01@gmail.com>
2023-12-11 23:30:32 +0100

    9

---
+++ Zadanie 9 Zasoby sieciowe/app/src/main/java/com/example/myapplication/BookContainer.java
@@ -0,0 +1,20 @@
+package com.example.myapplication;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
+public class BookContainer {
+
+    @SerializedName("docs")
+    private List<Book> bookList;
+
+    public List<Book> getBookList() {
+        return bookList;
+    }
+
+    public void setBookList(List<Book> bookList) {
+        this.bookList = bookList;
+    }
+    
+}
