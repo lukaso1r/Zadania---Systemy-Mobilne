1315f2b90131446ac117a1334a45b9952eed584b
Łukasz Kaliszewicz <lukasz.kaliszewicz01@gmail.com>
2023-12-12 01:35:54 +0100

    19

--- Zadanie 9 Zasoby sieciowe/app/build.gradle.kts
+++ Zadanie 9 Zasoby sieciowe/app/build.gradle.kts
@@ -4,12 +4,12 @@ plugins {
 
 android {
     namespace = "com.example.myapplication"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.myapplication"
         minSdk = 28
-        targetSdk = 33
+        targetSdk = 34
         versionCode = 1
         versionName = "1.0"
 
@@ -30,18 +30,34 @@ android {
 
 dependencies {
 
+//    implementation("androidx.appcompat:appcompat:1.6.1")
+//    implementation("com.google.android.material:material:1.10.0")
+//    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+//    testImplementation("junit:junit:4.13.2")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+
+    //Zadanie 6 - oryginalne
+//    implementation ("com.squareup.picasso:picasso:2.1.1")
+//    implementation ("com.squareup.retrofit2:retrofit:2.6.2")
+//    implementation ("androidx.recyclerview:recyclerview:1.2.1")
+//    implementation ("androidx.cardview:cardview:1.0.0")
+//    implementation ("com.squareup.retrofit2:converter-gson:2.6.2")
+//    implementation ("com.squareup.okhttp3:logging-interceptor:3.10.0")
+
+    //Zad6 Kuba get
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.squareup.picasso:picasso:2.8")
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("androidx.recyclerview:recyclerview:1.3.2")
+    implementation("androidx.cardview:cardview:1.0.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.android.material:material:1.10.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
-    //Zadanie 6
-    implementation ("com.squareup.picasso:picasso:2.1.1")
-    implementation ("com.squareup.retrofit2:retrofit:2.6.2")
-    implementation ("androidx.recyclerview:recyclerview:1.2.1")
-    implementation ("androidx.cardview:cardview:1.0.0")
-    implementation ("com.squareup.retrofit2:converter-gson:2.6.2")
-    implementation ("com.squareup.okhttp3:logging-interceptor:3.10.0")
 }
--- Zadanie 9 Zasoby sieciowe/app/src/main/java/com/example/myapplication/MainActivity.java
+++ Zadanie 9 Zasoby sieciowe/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.myapplication;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
@@ -9,14 +10,24 @@ import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.SearchView;
 import android.widget.TextView;
+import retrofit2.Callback;
 
+import com.google.android.material.snackbar.BaseTransientBottomBar;
+import com.google.android.material.snackbar.Snackbar;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,6 +36,64 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.book_menu, menu);
+
+        MenuItem searchItem = menu.findItem(R.id.menu_item_search);
+        final SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                fetchBooksData(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    private void fetchBooksData(String query) {
+        String finalQuery = prepareQuery(query);
+        BookService bookService = RetrofitInstance.getRetrofitInstance().create(BookService.class);
+
+        Call<BookContainer> booksApiCall = bookService.findBooks(finalQuery);
+
+        booksApiCall.enqueue(new Callback<BookContainer>() {
+            @Override
+            public void onResponse(Call<BookContainer> call, Response<BookContainer> response) {
+                if(response.body() != null) {
+                    setupBookListView(response.body().getBookList());
+                }
+            }
+            @Override
+            public void onFailure(Call<BookContainer> call, Throwable t) {
+//                Snackbar.make(findViewById(R.id.main_view),
+//                        getString(R.string.book_download_fail),
+//                        BaseTransientBottomBar.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private String prepareQuery(String query) {
+        String[] queryParts = query.split("\\s+");
+        return TextUtils.join("+", queryParts);
+    }
+
+    private void setupBookListView(List<Book> books) {
+        RecyclerView recyclerView = findViewById(R.id.recyclerview);
+        final BookAdapter adapter = new BookAdapter();
+        adapter.setBooks(books);
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+
 
     private class BookAdapter extends RecyclerView.Adapter<BookHolder> {
         private List<Book> books;
@@ -60,7 +129,7 @@ public class MainActivity extends AppCompatActivity {
 
     private class BookHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
-        private static final String IMAGE_URL_BASE = "http://http://covers.openlibrary.org/b/id/";
+        private static final String IMAGE_URL_BASE = "http://covers.openlibrary.org/b/id/";
 
         private final TextView bookTitleTextView;
         private final TextView bookAuthorTextView;
@@ -85,8 +154,8 @@ public class MainActivity extends AppCompatActivity {
                 numberOfPagesTextView.setText(book.getNumberOfPages());
                 cover = book.getCover();
                 if (book.getCover() != null)
-                    Picasso.with(itemView.getContext())
-                            .load(IMAGE_URL_BASE + book.getCover() + "-S.jpg")
+                    Picasso.get()
+                            .load(IMAGE_URL_BASE + book.getCover() + "-M.jpg")
                             .placeholder(R.drawable.baseline_menu_book_24)
                             .into(bookCover);
                 else
@@ -95,14 +164,19 @@ public class MainActivity extends AppCompatActivity {
         }
 
         @Override
-        public void onClick(View v) {
-            Intent intent = new Intent(MainActivity.this, BookDetailsActivity.class);
-            intent.putExtra("book_title", bookTitleTextView.getText());
-            intent.putExtra("book_author", bookAuthorTextView.getText());
-            intent.putExtra("book_number_og_pages", numberOfPagesTextView.getText());
-            intent.putExtra("book_cover", cover);
-            startActivity(intent);
+        public void onClick(View view) {
+
         }
+
+//        @Override
+//        public void onClick(View v) {
+//            Intent intent = new Intent(MainActivity.this, BookDetailsActivity.class);
+//            intent.putExtra("book_title", bookTitleTextView.getText());
+//            intent.putExtra("book_author", bookAuthorTextView.getText());
+//            intent.putExtra("book_number_og_pages", numberOfPagesTextView.getText());
+//            intent.putExtra("book_cover", cover);
+//            startActivity(intent);
+//        }
     }
 
     public boolean checkNullOrEmpty(String text) {
